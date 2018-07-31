7fd58e7cf3c9a06b5489b61c6e4cb08fed7f9f80
kevinai <kevinhwangai@126.com>
2018-07-31 19:48:44 +0800

    first commit

--- mainsite/models.py
+++ mainsite/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+class Post(models.Model):
+    title = models.CharField(max_length=200) #文章标题
+    slug = models.CharField(max_length=200) #文章网址
+    body = models.TextField() #文章内容
+    pub_date = models.DateTimeField(default=timezone.now) #发文时间
+
+    class Meta:
+        ordering = ('-pub_date',)
+
+        def __unicode__(self):
+            return title
+
+
